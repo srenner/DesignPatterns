aacf002dc5ec3440c37414b3f5ecf523df84b034
Shawn Renner <resume@shawnrenner.com>
2018-09-19 14:24:12 -0500

    start singleton

---
+++ SingletonPattern/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public class Configuration
+    {
+        private static Configuration _instance;
+
+        public int FavoriteNumber { get { return 6; } }
+
+        protected Configuration()
+        {
+
+        }
+
+        //not thread safe
+        public static Configuration Instance()
+        {
+            if(_instance == null)
+            {
+                _instance = new Configuration();
+            }
+            return _instance;
+        }
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Configuration configuration = Configuration.Instance();
+
+            Configuration configuration = null;
+
+            Console.WriteLine("Favorite number is " + configuration.FavoriteNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
